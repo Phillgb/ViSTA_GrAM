691297e5ba6afad3b1f2d64af3bdacf00700ad87
Phillipe Gauvin-Bourdon <phillipe.gauvinbourdon@gmail.com>
2018-09-07 16:31:42 -0400

    Testing ViSTA sensitivity

--- MBAH_scripts/Core_ViSTA/landscape_SETUP.py
+++ MBAH_scripts/Core_ViSTA/landscape_SETUP.py
@@ -14,8 +14,8 @@ import math
 #*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 
 #Grid
-Nr = 20 #Number of rows in grid (i.e. y axis)
-Nc = 20 #Number of columns in grid (i.e. x axis)
+Nr = 50 #Number of rows in grid (i.e. y axis)
+Nc = 50 #Number of columns in grid (i.e. x axis)
 cell_width = 1.0 #Width of a cell (m)
 manual_initialisation = 'off' #'on' or 'off' - On means the veg and sand grids are manually specified in the 'startgrids_manual' function; off means grids are specified in the 'startgrids' functions
 boundary_conditions = 'periodic' #'periodic' or 'open' - Periodic means sand that is lost from the edges is fed back in a Torus formation; 'open' means sand that falls off the edge of the grid is lost forever
@@ -24,7 +24,7 @@ start_grid_wall_mode = 'none' #'none', 'circle', 'square' or 'manual' - None mea
 start_grid_age_mode = 'random' #'uniform' or 'random' - Random means age is random up to 'start_grid_age'; 'uniform' means age is uniform over 'start_grid_age'
 start_grid_trunks_mode = 'uniform' #'uniform' or 'random' - Random means trunk proportion is random between 'min_trunk_proportion' and 'max_trunk_proportion'; 'uniform' means trunk proportion is uniform over 'start_grid_trunks'
 start_grid_porosity_mode = 'uniform' #'uniform' or 'random' - Random means plant porosities are random between 'min_random_porosity' and 'max_random_porosity'; 'uniform' means plant porosity is uniform over 'start_grid_porosity'
-start_grid_sand_upper = 10.0 #Highest depth of sediment when depths are randomly or uniformly assigned at start (m)
+start_grid_sand_upper = 0.2 #Highest depth of sediment when depths are randomly or uniformly assigned at start (m)
 start_grid_sand_lower = 10.0 #Lowest depth of sediment when depths are randomly assigned at start (m)
 start_grid_age = 360 #Age of plants when ages are randomly or uniformly assigned at start (months)
 start_grid_trunks = 0.7 #Proportion of tree height taken up by trunk, for equal proportion of total tree height that trunks take up (e.g. 0.5 = 50% of total tree height is trunk; 1.0 = 100% of total height is trunk)
@@ -33,36 +33,36 @@ sand_circle_radius = 20 #Radius of sand circle at the centre (if activated in 's
 sand_circle_height = 10 #Height of sand circle at the centre (if activated in 'start_grid_sand_mode') (m)
 wall_circle_or_square_radius_or_length = 5 #Radius of wall circle or half-length of square at the centre (if activated in 'start_grid_wall_mode') (m)
 wall_circle_or_square_height = 3. #Height of wall circle or square at the centre (if activated in 'start_grid_wall_mode') (m)
-veg_distrib = 0.1 # % of surface covered in vegetation, min = 0, max = 1.0 (i.e. 0.9 means 90% of cells have veg)
+veg_distrib = 0.9 # % of surface covered in vegetation, min = 0, max = 1.0 (i.e. 0.9 means 90% of cells have veg)
 grass_proportion = 0.4 #Proportion of the veg distribution that is grass (must sum to 1 with shrub and tree)
 shrub_proportion = 0.55 #Proportion of the veg distribution that is shrub (must sum to 1 with grass and tree)
 tree_proportion = 0.05 #Proportion of the veg distribution that is tree (must sum to 1 with grass and shrub)
 
 #Time
-model_iterations = 10 #Total number of iterations model is running
+model_iterations = 500 #Total number of iterations model is running
 wind_event_frequency = 1 #Frequency at which wind event occurs (i.e. 5 = one wind event every 5 model iterations)
-wind_resolution = 360 #Equivalent time for which wind is blowing during each wind event (MINUTES)
-veg_update_frequency = 100000000 #Frequency at which veg is updated using the veg patterning module (i.e. 10 = one update every 10 model iterations)
-veg_update_freq_equivalent = 12 #What the frequency of veg update represents in real MONTHS; must be divisible by 12 (1 = monthly (i.e. 12 updates a year), 6 = every 6 months (i.e. 2 updates a year), etc.)
-moisture_update_frequency = 1000000 #Frequency at which moisture is updated in feedback (i.e. 5 = one moisture update every 5 model iterations)
-moisture_update_frequency_equivalent = 1 #Only relevant for soil moisture feedback routine - it's what the moisture update frequency represents in real HOURS; technically should be compatible with veg update frequency and wind resolution
+wind_resolution = 12000  #Equivalent time for which wind is blowing during each wind event (MINUTES)
+veg_update_frequency = 5 #Frequency at which veg is updated using the veg patterning module (i.e. 10 = one update every 10 model iterations)
+veg_update_freq_equivalent = 3 #What the frequency of veg update represents in real MONTHS; must be divisible by 12 (1 = monthly (i.e. 12 updates a year), 6 = every 6 months (i.e. 2 updates a year), etc.)
+moisture_update_frequency = 1 #Frequency at which moisture is updated in feedback (i.e. 5 = one moisture update every 5 model iterations)
+moisture_update_frequency_equivalent = 200 #Only relevant for soil moisture feedback routine - it's what the moisture update frequency represents in real HOURS; technically should be compatible with veg update frequency and wind resolution
 
 #Wind
 wind_event_timeseries = 'constant' #Type of wind pattern imposed: can be 'constant', 'trend', 'v-shaped', 'weibull'
 windspeed_variability = 'off' #'on' adds variability to the unobstructed windspeed at the start of each iteration (according to windspeed_var_normal_stdev) - currently only normal distribution(give a Weibull option too??);'off' removes this
 windspeed_stochasticity = 'on' #'on' adds stochasticity to windspeed in each cell (according to the norm_dist_multiplier); 'off' removes this
-wind_event_constant = 6. #For 'constant' (m/s) <--- Unobstructed windspeed entering grid for that timestep
+wind_event_constant = 7.0 #For 'constant' (m/s) <--- Unobstructed windspeed entering grid for that timestep
 wind_event_start = 2; wind_event_end = 10 #For 'trend', 'v-shaped' (m/s) <--- Unobstructed windspeed entering grid for that timestep
 windspeed_threshold = 5.1 #Windspeed threshold over which sand transport occurs (m/s)
-wind_angle_array = [132] #Angle of wind entering grid for that time step (degrees) (Array is repeated the number of wind iterations)
+wind_angle_array = [212] #Angle of wind entering grid for that time step (degrees) (Array is repeated the number of wind iterations)
 weibull_shape_parameter = 1.6 #Shape parameter for Weibull distribution if 'wind_event_timeseries' is set to 'weibull'
 weibull_scale_parameter = 3.4 #Scale parameter for Weibull distribution if 'wind_event_timeseries' is set to 'weibull'
 
 #Rainfall
-rainfall_series_timeseries = 'seasonal' #Type of rainfall temporal pattern imposed: 'constant', 'trend', 'v-shaped', 'asym', 'red', 'seasonal'
+rainfall_series_timeseries = 'constant' #Type of rainfall temporal pattern imposed: 'constant', 'trend', 'v-shaped', 'asym', 'red', 'seasonal'
 rainfall_series_spatial = 'homogeneous' #Type of rainfall spatal pattern imposed: 'homogeneous', 'vertical' or 'corner'
 rainfall_variability = 'off' #'on' adds variability to the rainfall at the start of each iteration; 'off' removes this
-rainfall_series_constant = 400 #For constant stress on time series (mm, annual equivalent)
+rainfall_series_constant = 90 #For constant stress on time series (mm, annual equivalent)
 rainfall_series_constant_MAM = 57 #If rainfall_series_timeseries is 'seasonal'; March, April, May rainfall
 rainfall_series_constant_JJA = 11 #If rainfall_series_timeseries is 'seasonal'; June, July, August rainfall
 rainfall_series_constant_SON = 38 #If rainfall_series_timeseries is 'seasonal'; September, October, November rainfall
@@ -75,7 +75,7 @@ beta_start = 0.1; beta_end = 0.1 #Red noise annual variation parameter
 rainfall_series_gradient = 0.3 #For 'vertical' and 'corner' of spatial
 
 #Vegetation
-sed_balance_stress_switch = 'off' #'off' if sediment stress is always zero; 'on' if sediment stress is allowed to vary with sediment movement
+sed_balance_stress_switch = 'on' #'off' if sediment stress is always zero; 'on' if sediment stress is allowed to vary with sediment movement
 recolonisation_dynamism = 'on' #'off' means grass/tree/shrub proportions remain the same at every run; 'on' means plant recolonisation is dynamically dependent on current proportions of grass/shrub/tree in the domain
 max_height_grass = 1.0 #Maximum height a grass will ever reach if it has full biomass
 max_height_shrub = 1.5 #Maximum height a shrub will ever reach if it has full biomass
